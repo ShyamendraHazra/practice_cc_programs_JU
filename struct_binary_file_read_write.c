71e28babc279ee2eb0a59a3c8d1589214731c16e
Shyamendra Hazra <shyamendrahazra@gmail.com>
2024-12-12 09:23:16 +0530

    Thu Dec 12 09:23:16 AM IST 2024

---
+++ struct_binary_file_read_write.c
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define f1 "emplTEXT"
+#define f2 "emplBIN"
+#define TXT 'T'
+#define BIN 'B'
+
+
+typedef struct employee Employee;
+
+Employee {
+    char* name;
+    char* emp_id;
+    unsigned int salary;
+};
+
+void control();
+bool menu(FILE** fpT, FILE** fpB);
+void file_ops(FILE** fp, const char* filename, const char* mode);
+void display_files(FILE** fp, char mode);
+void append_to_files(FILE** fp, char mode);
+void delete_entry(FILE** fp, char mode, const int* entry_list, int entry_count);
+
+int main() {
+
+
+    return 0;
+}
+
+void control() {
+
+    FILE* empLTF = NULL;
+    FILE* empLBF = NULL;
+    bool flag = true;
+
+    while(flag) {
+        printf("Choose from below options \n");
+        flag = menu(&empLTF, &empLBF);
+    }
+
+}
+
+bool menu(FILE** fpT, FILE** fpB) {
+
+    bool sig = true;
+    int choice = -1;
+    
+    printf("\
+        Enter 1 to display the file\n\
+        Enter 2 to append an entry\n\
+        Enter 3 to delete any entry(ies)\n\
+        Enter 0 to exit...\n\
+        Your choice [1,2,3,0] :: 
+    ");
+
+    scanf("%d", &choice);
+
+    if(choice < 0 || choice > 3) {
+        
+        printf("Invalid choice please try again!\n\n");
+    
+    } else if(!choice) {
+        
+        printf("Exiting....");
+        sig = false;
+    
+    } else {
+
+        switch(choice) {
+            
+            case 1:
+                display_files(&fpT, TXT);
+                display_files(&fpB, BIN);
+                break;
+            
+            case 2: 
+                append_to_file(&fpT, TXT);
+                append_to_file(&fpB, BIN);
+                break;
+
+            case 3:
+                delete_entry(&fpT, TXT);           
+                delete_entry(&fpB, BIN);           
+                break;
+        }
+
+    }
+
+
+    return sig;
+}
+
+void file_ops(FILE** fp, const char* filename, const char* mode) {
+    
+    *fp = fopen(filename, mode);
+    
+    if(fp == NULL) {
+
+        perror("Error opening file %s", filename);
+        exit(EXIT_FAILURE);
+    }
+    
+}
+
+void display_files(FILE** fp, char mode) {
+
+    switch(mode) {
+        case TXT:
+            file_ops(fp, f1, "r");
+            
+            if(!feof(*fp)) {
+
+                int count = 0;
+                while(!feof()) {
+                    count++;
+                    printf("%d | ",count);
+                    printf("%c",fgetc(*fp));
+
+                }
+
+            } else {
+
+                printf("No entry in the file yet!!\n");
+
+            }
+            fclose(*fp);
+            *fp = NULL;
+            break;
+        
+        case BIN:
+            file_ops(fp, f1, "rb");
+
+            if(!feof(*fp)) {
+
+                int count = 0;
+                char ch;
+                while(!feof()) {
+                    count++;
+                    printf("%d | ",count);
+                    printf("%c",fread(&ch, sizeof(char), 1, *fp));
+
+                }
+
+            } else {
+
+                printf("No entry in the file yet!!\n");
+
+            }
+
+            fclose(*fp);
+            *fp = NULL;
+            break;
+    }
+
+}
+void append_to_files(FILE** fp, char mode) {
+    
+        switch(mode) {
+        case TXT:
+            
+            file_ops(fp, f1, "a");
+            
+            int count = 0;
+
+            Employee emp;
+
+
+            fclose(*fp);
+            *fp = NULL;
+            
+            break;
+        
+        case BIN:
+            file_ops(fp, f1, "rb");
+
+            if(!feof(*fp)) {
+
+                int count = 0;
+                char ch;
+                while(!feof()) {
+                    count++;
+                    printf("%d | ",count);
+                    printf("%c",fread(&ch, sizeof(char), 1, *fp));
+
+                }
+
+            } else {
+
+                printf("No entry in the file yet!!\n");
+
+            }
+
+            fclose(*fp);
+            *fp = NULL;
+            break;
+    }
+
+}
+void delete_entry(FILE** fp, char mode, const int* entry_list, int entry_count);
